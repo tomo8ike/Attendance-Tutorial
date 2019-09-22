dee8928526e5e277a1a08d3b79d4f7b9164279c9
t-ikeda <tomo8ike@gmail.com>
2019-09-22 01:50:15 +0000

    Finish login-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user # 保尊成功後、ログインします。
       # 保存に成功した場合は、ここに記述した処理が実行されます。
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
